9a56e248e90e6f8296dac6f30c7715804dd6de21
TomZ <tomz@freedommail.ch>
2017-01-10 16:42:35 +0100

    Add community page

--- generate.sh
+++ generate.sh
@@ -5,5 +5,5 @@ rm -rf devel howtos
 
 ikiwiki --setup setup
 
-mv html-temp/devel html-temp/howtos .
+mv html-temp/devel html-temp/howtos html-temp/community.html .
 rm -rf html-temp sources/.ikiwiki
