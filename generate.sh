e5d0cf88a84299b41fcf9a162084a35ab1187504
TomZ <tomz@freedommail.ch>
2017-02-20 13:12:02 +0100

    Import old releases

--- generate.sh
+++ generate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd `dirname $0`
 rm -rf html-temp
-rm -rf devel howtos
+rm -rf devel howtos news
 
 ikiwiki --setup setup
 
-mv html-temp/devel html-temp/howtos html-temp/community.html .
-rm -rf html-temp sources/.ikiwiki
+mv html-temp/devel html-temp/howtos html-temp/community.html html-temp/news .
+rm -rf html-temp
